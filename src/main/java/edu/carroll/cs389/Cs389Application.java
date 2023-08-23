cd708c3449737721ba7b49275f6a927a4eb595f3
Ryan Johnson <rjohnson@carroll.edu>
2023-08-23 16:17:08 -0600

    Initial commit

---
+++ src/main/java/edu/carroll/cs389/Cs389Application.java
@@ -0,0 +1,13 @@
+package edu.carroll.cs389;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs389Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs389Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/carroll/cs389/Cs389ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.carroll.cs389;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs389ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
