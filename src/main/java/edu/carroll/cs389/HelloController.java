aede0a0483307416aa3057d6937d3e1f420907eb
Ryan Johnson <rjohnson@carroll.edu>
2023-08-23 21:55:21 -0600

    Dynamic HTML rendering

--- src/main/java/edu/carroll/cs389/HelloController.java
+++ src/main/java/edu/carroll/cs389/HelloController.java
@@ -1,12 +1,15 @@
 package edu.carroll.cs389;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    @GetMapping("/hello")
-    public String index() {
+    @GetMapping("/")
+    public String index(@RequestParam(value = "name", defaultValue = "Student")String name, Model model) {
+        model.addAttribute("name", name);
         return "index";
     }
 }
