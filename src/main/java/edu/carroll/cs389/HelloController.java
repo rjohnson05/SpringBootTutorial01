a8e0786451cd388f80bfe7b9c310b73876cb8860
Ryan Johnson <rjohnson@carroll.edu>
2023-08-23 21:39:28 -0600

    HTML response

--- src/main/java/edu/carroll/cs389/HelloController.java
+++ src/main/java/edu/carroll/cs389/HelloController.java
@@ -1,12 +1,12 @@
 package edu.carroll.cs389;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class HelloController {
     @GetMapping("/hello")
     public String index() {
-        return "Hello from Spring Boot!";
+        return "index";
     }
 }
