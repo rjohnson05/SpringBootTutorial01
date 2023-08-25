2d8f75d8db84efc4b884960fce88b14d5f3a6a49
Ryan Johnson <rjohnson@carroll.edu>
2023-08-25 13:34:11 -0600

    Addition of logging

--- src/main/java/edu/carroll/cs389/service/LoginServiceImpl.java
+++ src/main/java/edu/carroll/cs389/service/LoginServiceImpl.java
@@ -2,14 +2,16 @@ package edu.carroll.cs389.service;
 
 import edu.carroll.cs389.jpa.model.Login;
 import edu.carroll.cs389.jpa.repo.LoginRepository;
-import edu.carroll.cs389.web.form.LoginForm;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 @Service
 public class LoginServiceImpl implements LoginService {
     private final LoginRepository loginRepo;
+    private static final Logger log = LoggerFactory.getLogger(LoginServiceImpl.class);
 
     public LoginServiceImpl(LoginRepository loginRepo) {
         this.loginRepo = loginRepo;
@@ -23,20 +25,27 @@ public class LoginServiceImpl implements LoginService {
      */
     @Override
     public boolean validateUser(String username, String password) {
+        log.debug("validateUser: user '{}' attempted login", username);
+
         // Always do the lookup in a case-insensitive manner (lower-casing the data).
         List<Login> users = loginRepo.findByUsernameIgnoreCase(username);
 
         // We expect 0 or 1, so if we get more than 1, bail out as this is an error we don't deal with properly.
-        if (users.size() != 1)
+        if (users.size() != 1) {
+            log.debug("validateUser: found {} users", users.size());
             return false;
+        }
         Login u = users.get(0);
         // XXX - Using Java's hashCode is wrong on SO many levels, but is good enough for demonstration purposes.
         // NEVER EVER do this in production code!
         final String userProvidedHash = Integer.toString(password.hashCode());
-        if (!u.getHashedPassword().equals(userProvidedHash))
+        if (!u.getHashedPassword().equals(userProvidedHash)) {
+            log.debug("validateUser: password !match");
             return false;
+        }
 
         // User exists, and the provided password matches the hashed password in the database.
+        log.info("validateUser: successful login for {}", username);
         return true;
     }
 }
